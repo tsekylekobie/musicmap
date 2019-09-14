cc544c4339d0cae20890886388ed4ffb8f6d92c6
tsekylekobie <tse.kylekobie@berkeley.edu>
2019-09-14 14:30:01 -0400

    added sidebar component

---
+++ my-app/src/Sidebar.react.js
@@ -0,0 +1,7 @@
+import React, { Component } from 'react';
+
+export default class Sidebar extends Component {
+    render() {
+        return <div style={{float: 'right'}}>Hello world</div>
+    }
+}
