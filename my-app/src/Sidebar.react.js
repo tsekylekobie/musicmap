1d5bd4de95658f9430a045ebfea1a302d494b1a9
tsekylekobie <tse.kylekobie@berkeley.edu>
2019-09-14 14:44:15 -0400

    added material-ui

--- my-app/src/Sidebar.react.js
+++ my-app/src/Sidebar.react.js
@@ -1,7 +1,12 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import TextField from "@material-ui/core/TextField";
 
 export default class Sidebar extends Component {
-    render() {
-        return <div style={{float: 'right'}}>Hello world</div>
-    }
+  render() {
+    return (
+      <div style={{ float: "right" }}>
+        <TextField placeholder="Placeholder" />
+      </div>
+    );
+  }
 }
