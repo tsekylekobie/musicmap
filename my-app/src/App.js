3825768fc1eb116eacf7e627601514bff0d9d1e3
Kentaro Tanaka <kentnk@Kentaros-MacBook-Pro.local>
2019-09-14 21:25:53 -0400

    some map coloring

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Sidebar from './Sidebar.react';
+import CountryMap from './map.js';
 import './App.css';
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
       <div style={{float:'left'}}>Music Map</div>
       <Sidebar/>
       <iframe src="https://open.spotify.com/embed/playlist/37i9dQZEVXbLRQDuF5jeBp" width="300" height="380" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
+      <CountryMap />
     </div>
   );
 }
