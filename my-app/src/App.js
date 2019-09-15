2388f2c5b04d5386a51cc22540a924b55b598b2f
Kentaro Tanaka <kentnk@Kentaros-MacBook-Pro.local>
2019-09-14 21:46:01 -0400

    tried to fix bugs but it didn't work

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -17,6 +17,20 @@ function App() {
         allow="encrypted-media"
       ></iframe>
       <CountryMap />
+      <h1 style={{ margin: 0 }}>Music Map</h1>
+      <Sidebar />
+      <Playlist
+        embedCode={
+          <iframe
+            src="https://open.spotify.com/embed/playlist/37i9dQZEVXbLRQDuF5jeBp"
+            width="300"
+            height="380"
+            frameborder="0"
+            allowtransparency="true"
+            allow="encrypted-media"
+          ></iframe>
+        }
+      />
     </div>
   );
 }
