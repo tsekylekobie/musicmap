881ae267e256debc72246cc148317c1652db58d3
tsekylekobie <tse.kylekobie@berkeley.edu>
2019-09-15 09:34:10 -0400

    display map

--- my-app/src/Sidebar.react.js
+++ my-app/src/Sidebar.react.js
@@ -32,7 +32,7 @@ export default class Sidebar extends Component {
         Authorization:
           // Get OAuth Token from:
           // https://developer.spotify.com/console/get-playlist/?playlist_id=59ZbFPES4DQwEjBpWHzrtC&market=&fields=
-          "Bearer BQDRMUARNNTbW1Lti1n_TjdvoE293rDpv9HdTIdv6Cm2fhGNB3IZZ4s73s0t4_tWtmfFM6G7TwVFKcnN1aYnz0UYVFYmvOXHmeeq7W-LTHPvCf9QRbz0JNteT_gQ0oWgVlme17i-SIaGGC-ECg"
+          "Bearer BQD94xic1aEl_t_2aRYw_k5mTkeehM9JqnhNvJHNGjqnXo7wrg68VJTdXH0T9EzApVr6Nwb8OTFY325--OkbJlf7q_NhXj9sJpwn1jmge9bqMkKBqktHOh5BELSNOBOiK7-tzXMUpgKIPKCjgWu2xg"
       }
     })
       .then(response => {
